792622b0983fbfea9101634e484a95077e4190d3
VaishnaviKB1999 <43808547+VaishnaviKB1999@users.noreply.github.com>
2018-10-03 14:42:09 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sum
+{
+	public static void main(String args[])
+	{
+	int a,b;
+	int sum=0;
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter a,b values:");
+	a=s.nextInt();
+	b=s.nextInt();
+	sum=a+b;
+	System.out.println("Sum:"+sum);
+	}
+}
+	
